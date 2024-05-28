6b7792c4925e67b044dd33ac08de0aede73f240b
mahibashardeveloper <mahibashar2023@gmail.com>
2024-05-28 18:43:51 +0600

    upload npm package

---
+++ index.js
@@ -0,0 +1,12 @@
+import Freecurrencyapi from "@everapi/freecurrencyapi-js";
+
+const freecurrencyapi = new Freecurrencyapi('fca_live_j6x8dN1S7seusP5zESVOOYauJZJhsvBSRaaRZ0tH');
+
+export async function convertCurrency(formCurrency, toCurrency, units) {
+    const res = await freecurrencyapi.latest({
+        base_currency: formCurrency,
+        currencies: toCurrency
+    });
+    const multiplier = res.data[toCurrency];
+    return multiplier*units
+}
